YanziNetworks/logstash-input-tcp

commit e4d4510381b427de8476df82d63961e5031fd532

#7 Nio Plain Socket Server
Fixes #71

lib/logstash/inputs/tcp.rb
@@ -1,9 +1,16 @@
-  # encoding: utf-8
+# encoding: utf-8
+
+require "java"
+
 require "logstash/inputs/base"
 require "logstash/util/socket_peer"
+require "logstash-input-tcp_jars"
+require "logstash/inputs/tcp/decoder_impl"
 
 require "socket"
 require "openssl"
+
+java_import org.logstash.tcp.InputLoop
 
 # Read events over a TCP socket.
 #
@@ -128,14 +135,25 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     # in the close method even if we also close it in the stop method since we could have
     # a situation where register is called but not run & stop.
 
-    self.server_socket = new_server_socket if server?
+    if server?
+      if @ssl_enable
+        self.server_socket = new_server_socket
+      else
+        @loop = InputLoop.new(@host, @port, DecoderImpl.new(@codec, self))
+      end
+    end
   end
 
   def run(output_queue)
+    @output_queue = output_queue
     if server?
-      run_server(output_queue)
+      if @ssl_enable
+        run_ssl_server
+      else
+        @loop.run
+      end
     else
-      run_client(output_queue)
+      run_client()
     end
   end
 
@@ -144,6 +162,7 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     # and any thread using them will exit.
     # catch all rescue nil on close to discard any close errors or invalid socket
     server_socket.close rescue nil
+    @loop.close rescue nil
     client_socket.close rescue nil
     connection_sockets.each{|socket| socket.close rescue nil}
   end
@@ -154,16 +173,27 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     # register called but never run & stop, only close.
     # catch all rescue nil on close to discard any close errors or invalid socket
     server_socket.close rescue nil
+    @loop.close rescue nil
+  end
+
+  def decode_buffer(client_address, client_port, codec, proxy_address, proxy_port, tbuf)
+    codec.decode(tbuf) do |event|
+      if @proxy_protocol
+        event.set(PROXY_HOST_FIELD, proxy_address) unless event.get(PROXY_HOST_FIELD)
+        event.set(PROXY_PORT_FIELD, proxy_port) unless event.get(PROXY_PORT_FIELD)
+      end
+      enqueue_decorated(event, client_address, client_port)
+    end
   end
 
   private
 
-  def run_server(output_queue)
+  def run_ssl_server()
     while !stop?
       begin
         socket = add_connection_socket(server_socket.accept)
         # start a new thread for each connection.
-        server_connection_thread(output_queue, socket)
+        server_connection_thread(socket)
       rescue OpenSSL::SSL::SSLError => e
         # log error, close socket, accept next connection
         @logger.debug? && @logger.debug("SSL Error", :exception => e, :backtrace => e.backtrace)
@@ -178,35 +208,37 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     server_socket.close rescue nil
   end
 
-  def run_client(output_queue)
+  def run_client()
     while !stop?
       self.client_socket = new_client_socket
-      handle_socket(client_socket, client_socket.peeraddr[3], client_socket.peeraddr[1], output_queue, @codec.clone)
+      handle_socket(client_socket)
     end
   ensure
     # catch all rescue nil on close to discard any close errors or invalid socket
     client_socket.close rescue nil
   end
 
-  def server_connection_thread(output_queue, socket)
-    Thread.new(output_queue, socket) do |q, s|
+  def server_connection_thread(socket)
+    Thread.new(socket) do |s|
       begin
         @logger.debug? && @logger.debug("Accepted connection", :client => s.peer, :server => "#{@host}:#{@port}")
-        handle_socket(s, s.peeraddr[3], s.peeraddr[1], q, @codec.clone)
+        handle_socket(s)
       ensure
         delete_connection_socket(s)
       end
     end
   end
 
-  def handle_socket(socket, client_address, client_port, output_queue, codec)
+  def handle_socket(socket)
+    client_address = socket.peeraddr[3]
+    client_port = socket.peeraddr[1]
     peer = "#{client_address}:#{client_port}"
     first_read = true
+    codec = @codec.clone
     while !stop?
-      tbuf = read(socket)
+      tbuf = socket.sysread(16384)
       if @proxy_protocol && first_read
         first_read = false
-        orig_buf = tbuf
         pp_hdr, tbuf = tbuf.split("\r\n", 2)
 
         pp_info = pp_hdr.split(/\s/)
@@ -221,18 +253,7 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
           client_port = pp_info[4]
         end
       end
-      codec.decode(tbuf) do |event|
-        if @proxy_protocol
-          event.set(PROXY_HOST_FIELD, proxy_address) unless event.get(PROXY_HOST_FIELD)
-          event.set(PROXY_PORT_FIELD, proxy_port) unless event.get(PROXY_PORT_FIELD)
-        end
-        event.set(HOST_FIELD, client_address) unless event.get(HOST_FIELD)
-        event.set(PORT_FIELD, client_port) unless event.get(PORT_FIELD)
-        event.set(SSLSUBJECT_FIELD, socket.peer_cert.subject.to_s) if @ssl_enable && @ssl_verify && event.get(SSLSUBJECT_FIELD).nil?
-
-        decorate(event)
-        output_queue << event
-      end
+      decode_buffer(client_address, client_port, codec, proxy_address, proxy_port, tbuf)
     end
   rescue EOFError
     @logger.debug? && @logger.debug("Connection closed", :client => peer)
@@ -250,36 +271,19 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     socket.close rescue nil
 
     codec.respond_to?(:flush) && codec.flush do |event|
-      event.set(HOST_FIELD, client_address) unless event.get(HOST_FIELD)
-      event.set(PORT_FIELD, client_port) unless event.get(PORT_FIELD)
-      event.set(SSLSUBJECT_FIELD, socket.peer_cert.subject.to_s) if @ssl_enable && @ssl_verify && event.get(SSLSUBJECT_FIELD).nil?
-
-      decorate(event)
-      output_queue << event
+      enqueue_decorated(event, client_address, client_port)
     end
   end
 
-  private
-  def client_thread(output_queue, socket)
-    Thread.new(output_queue, socket) do |q, s|
-      begin
-        @logger.debug? && @logger.debug("Accepted connection", :client => s.peer, :server => "#{@host}:#{@port}")
-        handle_socket(s, s.peeraddr[3], s.peeraddr[1], q, @codec.clone)
-      rescue Interrupted
-        s.close rescue nil
-      ensure
-        @client_threads_lock.synchronize{@client_threads.delete(Thread.current)}
-      end
-    end
+  def enqueue_decorated(event, client_address, client_port)
+    event.set(HOST_FIELD, client_address) unless event.get(HOST_FIELD)
+    event.set(PORT_FIELD, client_port) unless event.get(PORT_FIELD)
+    decorate(event)
+    @output_queue << event
   end
-
-  private
+  
   def server?
     @mode == "server"
-  end
-
-  def read(socket)
-    socket.sysread(16384)
   end
 
   def ssl_context

.gitignore
@@ -3,3 +3,8 @@ Gemfile.lock
 .bundle
 vendor
 .idea
+build
+.gradle
+*.iml
+*.iws
+*.ipr

Rakefile
@@ -4,4 +4,19 @@ task :default do
   system("rake -T")
 end
 
-require "logstash/devutils/rake"
+require 'logstash/devutils/rake'
+require 'jars/installer'
+
+task :install_jars do
+  system('./gradlew vendor')
+end
+
+task :vendor => :install_jars
+
+task :test do
+  require 'rspec/core/runner'
+  require 'rspec'
+  system './gradlew clean test'
+  Rake::Task[:install_jars].invoke
+  exit(RSpec::Core::Runner.run(Rake::FileList['spec/**/*_spec.rb']))
+end

build.gradle
@@ -1,0 +1,68 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+apply plugin: "java"
+apply plugin: 'maven'
+apply plugin: "distribution"
+apply plugin: "idea"
+
+group "org.logstash.inputs"
+version Files.readAllLines(Paths.get("version")).first()
+
+sourceCompatibility = JavaVersion.VERSION_1_8
+
+buildscript {
+  repositories {
+    mavenCentral()
+    jcenter()
+  }
+
+  dependencies {
+    classpath 'com.github.jengelman.gradle.plugins:shadow:1.2.4'
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+task wrapper(type: Wrapper) {
+  gradleVersion = '4.0'
+}
+
+apply plugin: 'com.github.johnrengelman.shadow'
+
+shadowJar {
+  classifier = null
+}
+
+dependencies {
+  compile 'io.netty:netty-all:4.1.3.Final'
+  compile group: 'commons-io', name: 'commons-io', version: '2.5'
+}
+
+task vendor(dependsOn: shadowJar) << {
+  String vendorPathPrefix = "vendor/jar-dependencies"
+  String projectGroupPath = project.group.replaceAll('\\.', '/')
+  File projectJarFile = file("${vendorPathPrefix}/${projectGroupPath}/${project.name}/${project.version}/${project.name}-${project.version}.jar")
+  projectJarFile.mkdirs()
+  Files.copy(file("$buildDir/libs/${project.name}-${project.version}.jar").toPath(), projectJarFile.toPath(), REPLACE_EXISTING)
+}

gradle/wrapper/gradle-wrapper.properties
@@ -1,0 +1,6 @@
+#Wed Jun 21 11:39:16 CEST 2017
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-4.0-all.zip

gradlew
@@ -1,0 +1,172 @@
+#!/usr/bin/env sh
+
+##############################################################################
+##
+##  Gradle start up script for UN*X
+##
+##############################################################################
+
+# Attempt to set APP_HOME
+# Resolve links: $0 may be a link
+PRG="$0"
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`/" >/dev/null
+APP_HOME="`pwd -P`"
+cd "$SAVED" >/dev/null
+
+APP_NAME="Gradle"
+APP_BASE_NAME=`basename "$0"`
+
+# Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+DEFAULT_JVM_OPTS=""
+
+# Use the maximum available, or set MAX_FD != -1 to use that value.
+MAX_FD="maximum"
+
+warn () {
+    echo "$*"
+}
+
+die () {
+    echo
+    echo "$*"
+    echo
+    exit 1
+}
+
+# OS specific support (must be 'true' or 'false').
+cygwin=false
+msys=false
+darwin=false
+nonstop=false
+case "`uname`" in
+  CYGWIN* )
+    cygwin=true
+    ;;
+  Darwin* )
+    darwin=true
+    ;;
+  MINGW* )
+    msys=true
+    ;;
+  NONSTOP* )
+    nonstop=true
+    ;;
+esac
+
+CLASSPATH=$APP_HOME/gradle/wrapper/gradle-wrapper.jar
+
+# Determine the Java command to use to start the JVM.
+if [ -n "$JAVA_HOME" ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+        # IBM's JDK on AIX uses strange locations for the executables
+        JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+        JAVACMD="$JAVA_HOME/bin/java"
+    fi
+    if [ ! -x "$JAVACMD" ] ; then
+        die "ERROR: JAVA_HOME is set to an invalid directory: $JAVA_HOME
+
+Please set the JAVA_HOME variable in your environment to match the
+location of your Java installation."
+    fi
+else
+    JAVACMD="java"
+    which java >/dev/null 2>&1 || die "ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+
+Please set the JAVA_HOME variable in your environment to match the
+location of your Java installation."
+fi
+
+# Increase the maximum file descriptors if we can.
+if [ "$cygwin" = "false" -a "$darwin" = "false" -a "$nonstop" = "false" ] ; then
+    MAX_FD_LIMIT=`ulimit -H -n`
+    if [ $? -eq 0 ] ; then
+        if [ "$MAX_FD" = "maximum" -o "$MAX_FD" = "max" ] ; then
+            MAX_FD="$MAX_FD_LIMIT"
+        fi
+        ulimit -n $MAX_FD
+        if [ $? -ne 0 ] ; then
+            warn "Could not set maximum file descriptor limit: $MAX_FD"
+        fi
+    else
+        warn "Could not query maximum file descriptor limit: $MAX_FD_LIMIT"
+    fi
+fi
+
+# For Darwin, add options to specify how the application appears in the dock
+if $darwin; then
+    GRADLE_OPTS="$GRADLE_OPTS \"-Xdock:name=$APP_NAME\" \"-Xdock:icon=$APP_HOME/media/gradle.icns\""
+fi
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin ; then
+    APP_HOME=`cygpath --path --mixed "$APP_HOME"`
+    CLASSPATH=`cygpath --path --mixed "$CLASSPATH"`
+    JAVACMD=`cygpath --unix "$JAVACMD"`
+
+    # We build the pattern for arguments to be converted via cygpath
+    ROOTDIRSRAW=`find -L / -maxdepth 1 -mindepth 1 -type d 2>/dev/null`
+    SEP=""
+    for dir in $ROOTDIRSRAW ; do
+        ROOTDIRS="$ROOTDIRS$SEP$dir"
+        SEP="|"
+    done
+    OURCYGPATTERN="(^($ROOTDIRS))"
+    # Add a user-defined pattern to the cygpath arguments
+    if [ "$GRADLE_CYGPATTERN" != "" ] ; then
+        OURCYGPATTERN="$OURCYGPATTERN|($GRADLE_CYGPATTERN)"
+    fi
+    # Now convert the arguments - kludge to limit ourselves to /bin/sh
+    i=0
+    for arg in "$@" ; do
+        CHECK=`echo "$arg"|egrep -c "$OURCYGPATTERN" -`
+        CHECK2=`echo "$arg"|egrep -c "^-"`                                 ### Determine if an option
+
+        if [ $CHECK -ne 0 ] && [ $CHECK2 -eq 0 ] ; then                    ### Added a condition
+            eval `echo args$i`=`cygpath --path --ignore --mixed "$arg"`
+        else
+            eval `echo args$i`="\"$arg\""
+        fi
+        i=$((i+1))
+    done
+    case $i in
+        (0) set -- ;;
+        (1) set -- "$args0" ;;
+        (2) set -- "$args0" "$args1" ;;
+        (3) set -- "$args0" "$args1" "$args2" ;;
+        (4) set -- "$args0" "$args1" "$args2" "$args3" ;;
+        (5) set -- "$args0" "$args1" "$args2" "$args3" "$args4" ;;
+        (6) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" ;;
+        (7) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" ;;
+        (8) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" ;;
+        (9) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" "$args8" ;;
+    esac
+fi
+
+# Escape application args
+save () {
+    for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/" ; done
+    echo " "
+}
+APP_ARGS=$(save "$@")
+
+# Collect all arguments for the java command, following the shell quoting and substitution rules
+eval set -- $DEFAULT_JVM_OPTS $JAVA_OPTS $GRADLE_OPTS "\"-Dorg.gradle.appname=$APP_BASE_NAME\"" -classpath "\"$CLASSPATH\"" org.gradle.wrapper.GradleWrapperMain "$APP_ARGS"
+
+# by default we should be in the correct project dir, but when run from Finder on Mac, the cwd is wrong
+if [ "$(uname)" = "Darwin" ] && [ "$HOME" = "$PWD" ]; then
+  cd "$(dirname "$0")"
+fi
+
+exec "$JAVACMD" "$@"

gradlew.bat
@@ -1,0 +1,84 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega

commit 37ef51e58185e08a69248ac1a5a99dce55657154

Fix not adding sslsubject to event
Commit e4d4510 accidentally prevents sslsubject from being added to the event
when using client certificates. This puts sslsubject back.

lib/logstash/inputs/tcp.rb
@@ -176,14 +176,14 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     @loop.close rescue nil
   end
 
-  def decode_buffer(client_ip_address, client_address, client_port, codec, proxy_address,
+  def decode_buffer(client_ip_address, client_address, client_port, ssl_subject_str, codec, proxy_address,
                     proxy_port, tbuf)
     codec.decode(tbuf) do |event|
       if @proxy_protocol
         event.set(PROXY_HOST_FIELD, proxy_address) unless event.get(PROXY_HOST_FIELD)
         event.set(PROXY_PORT_FIELD, proxy_port) unless event.get(PROXY_PORT_FIELD)
       end
-      enqueue_decorated(event, client_ip_address, client_address, client_port)
+      enqueue_decorated(event, client_ip_address, client_address, client_port, ssl_subject_str)
     end
   end
 
@@ -238,6 +238,12 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     client_address = socket.peeraddr[3]
     client_ip_address = socket.peeraddr[2]
     client_port = socket.peeraddr[1]
+    ssl_subject_str = nil
+
+    if @ssl_enable && @ssl_verify
+      ssl_subject_str = socket.peer_cert.subject.to_s
+    end
+
     peer = "#{client_address}:#{client_port}"
     first_read = true
     codec = @codec.clone
@@ -260,7 +266,7 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
           client_ip_address = ''
         end
       end
-      decode_buffer(client_ip_address, client_address, client_port, codec, proxy_address,
+      decode_buffer(client_ip_address, client_address, client_port, ssl_subject_str, codec, proxy_address,
                     proxy_port, tbuf)
     end
   rescue EOFError
@@ -279,14 +285,15 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     socket.close rescue nil
 
     codec.respond_to?(:flush) && codec.flush do |event|
-      enqueue_decorated(event, client_ip_address, client_address, client_port)
+      enqueue_decorated(event, client_ip_address, client_address, client_port, ssl_subject_str)
     end
   end
 
-  def enqueue_decorated(event, client_ip_address, client_address, client_port)
+  def enqueue_decorated(event, client_ip_address, client_address, client_port, ssl_subject_str)
     event.set(HOST_FIELD, client_address) unless event.get(HOST_FIELD)
     event.set(HOST_IP_FIELD, client_ip_address) unless event.get(HOST_IP_FIELD)
     event.set(PORT_FIELD, client_port) unless event.get(PORT_FIELD)
+    event.set(SSLSUBJECT_FIELD, ssl_subject_str) unless event.get(SSLSUBJECT_FIELD) or ssl_subject_str.nil?
     decorate(event)
     @output_queue << event
   end
@@ -316,7 +323,7 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
 
   def load_cert_store
     cert_store = OpenSSL::X509::Store.new
-    cert_store.set_default_paths
+    #cert_store.set_default_paths
     @ssl_extra_chain_certs.each do |cert|
       cert_store.add_file(cert)
     end